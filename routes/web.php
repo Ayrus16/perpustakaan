762355578e33b7bd106276b080de4b1580186158
Ayrus16 <suryap.sp83@gmail.com>
2023-10-05 13:43:07 +0700

    Configurasi redis

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Redis;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/', function () {
+//    $redis = Redis::incr('p');
+//    return $redis;
+// });
